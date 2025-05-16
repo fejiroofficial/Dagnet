2ac3844fa421efd57fc701ee86091978b1ce35c4
Fejiro Gospel <fejiro.gospel@gmail.com>
2025-05-16 07:50:00 +0100

    fix: height

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ const formId = "0000000";
 
 export default function LeadMag2() {
   return (
-    <div className="relative flex flex-col items-center lg:py-12 lg:h-screen gap-6 lg:justify-between overflow-hidden">
+    <div className="relative flex flex-col items-center lg:py-12 gap-6 lg:justify-between overflow-hidden">
       <HeapAnalytics />
       <Image
         src={background}
